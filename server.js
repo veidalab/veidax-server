f0df908318864aa8b995de631a178013dedc9b37
Brad Stimpson <brad.stimpson@gmail.com>
2016-11-08 22:23:16 -0500

    added move and disconnect to server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //SteamVR Social Nodejs server
 var io = require('socket.io')(process.env.PORT || 3000);
+var playerCount;
 
 console.log('server started');
 
@@ -7,9 +8,21 @@ io.on('connection', function(socket){
     console.log('client connected, broadcasting spawn');
 
     socket.broadcast.emit('spawn');
-    
+    playerCount++;
+
+    for(i=0;i<playerCount;i++){
+        socekt.emit('spawn');
+        console.log('sending spawn to the new player');
+    }
+
     socket.on('move', function(data) {
-        console.log('Player moved');
+        console.log('Player moved',JSON.stringify(data));
+        socket.broadcast.emit('move', data);
+    });
+
+    socket.on('disconnect', function() {
+        console.log('client disconnected');
+        playerCount--;
     });
 });
 
