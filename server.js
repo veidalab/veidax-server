cdcfc582b2c31a33a15a66a3b8110a8c811a0402
Brad Stimpson <brad.stimpson@gmail.com>
2016-11-09 18:07:19 -0500

    added uuid for each player

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 //SteamVR Social Nodejs server
 var io = require('socket.io')(process.env.PORT || 3000);
-var playerCount;
+var shortid = require('shortid');
 
 console.log('server started');
 
+var playerCount=0;
+
 io.on('connection', function(socket){
-    console.log('client connected, broadcasting spawn');
+    var thisClientId = shortid.generate();
+
+    console.log('client connected, broadcasting spawn, id:', thisClientId);
 
     socket.broadcast.emit('spawn');
     playerCount++;
 
     for(i=0;i<playerCount;i++){
-        socekt.emit('spawn');
+        socket.emit('spawn');
         console.log('sending spawn to the new player');
     }
 
     socket.on('move', function(data) {
+        data.id = thisClientId;
         console.log('Player moved',JSON.stringify(data));
         socket.broadcast.emit('move', data);
     });
