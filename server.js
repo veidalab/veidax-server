4cfed32431d662f2d44373f5b9f159f223e660d0
Brad Stimpson <brad.stimpson@gmail.com>
2016-11-17 21:56:25 -0500

    add disconnect function and remove playercount

--- server.js
+++ server.js
@@ -4,31 +4,41 @@ var shortid = require('shortid');
 
 console.log('server started');
 
-var playerCount=0;
+var players=[];
 
 io.on('connection', function(socket){
-    var thisClientId = shortid.generate();
+    var thePlayerId = shortid.generate();
+    
+    players.push(thePlayerId);
 
-    console.log('client connected, broadcasting spawn, id:', thisClientId);
+    console.log('client connected, broadcasting spawn:', { id: thePlayerId });
 
-    socket.broadcast.emit('spawn', { id: thisClientId });
-    playerCount++;
+    io.emit('spawn', { id: thePlayerId });
+    io.emit('requestPosition'); 
 
-   for(i=0;i<playerCount;i++){
-       socket.emit('spawn', { id: thisClientId });
-       console.log('sending spawn to connected clients');
-   }
+//    players.forEach(function(playerId){
+//        if(playerId == thePlayerId)
+//            return;
+//
+//        socket.emit('spawn', { id: playerId });
+//        console.log('sending spawn to connected clients', playerId);
+//    });
 
     socket.on('move', function(data) {
-        data.id = thisClientId;
+        data.id = thePlayerId;
         console.log('Player moved',JSON.stringify(data));
         socket.broadcast.emit('move', data);
     });
+    
+    socket.on('updatePosition', function(data) {
+        data.id = thePlayerId;
+        console.log('update position', data);
+        socket.broadcast.emit('updatePosition', data);
+    });
 
     socket.on('disconnect', function() {
-        console.log('client disconnected');
-        playerCount--;
+        console.log('client disconnected: ', { id: thePlayerId });
+        players.splice(players.indexOf(thePlayerId),1);
+        socket.broadcast.emit('disconnected',{ id: thePlayerId });
     });
 });
-
-
